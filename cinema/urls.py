24d338c8bf9dc99154c3f7defe16c3200a7ba1be
misha-cw <bigun.mykhaylo@gmail.com>
2025-11-06 14:44:13 +0200

    Refactor movie endpoints to use MovieViewSet and update URL routing

--- cinema/urls.py
+++ cinema/urls.py
@@ -1,14 +1,17 @@
-from django.urls import path
+from django.urls import path, include
+from rest_framework import routers
 
 from cinema.views import (
-    movie_list,
-    movie_detail,
+    MovieViewSet,
     GenreList,
     GenreDetail,
     ActorList,
     ActorDetail,
     CinemaHallViewSet,
 )
+
+router = routers.DefaultRouter()
+router.register("movies", MovieViewSet)
 
 cinema_hall_list = CinemaHallViewSet.as_view(
     actions={
@@ -27,8 +30,7 @@ cinema_hall_detail = CinemaHallViewSet.as_view(
 )
 
 urlpatterns = [
-    path("movies/", movie_list, name="movie-list"),
-    path("movies/<int:pk>/", movie_detail, name="movie-detail"),
+    path("", include(router.urls)),
     path("genres/", GenreList.as_view(), name="genre-list"),
     path("genres/<int:pk>/", GenreDetail.as_view(), name="genre-detail"),
     path("actors/", ActorList.as_view(), name="actor-list"),
